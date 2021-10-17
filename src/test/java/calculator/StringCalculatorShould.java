8b7ecbe88ca91ef5cd210422d17416c0b7561412
Nikhil Shinde <nikpshinde5@gmail.com>
2021-10-17 16:13:21 +0530

    Test case for empty string and return 0

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
+    
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
-
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    public void testEmptyString() {
+        //StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, StringCalculator.add(""));
     }
 }
