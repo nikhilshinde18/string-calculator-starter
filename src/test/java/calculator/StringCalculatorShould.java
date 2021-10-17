69805c59e1b2ec0a6c50369f63b43f4d01824493
Nikhil Shinde <nikpshinde5@gmail.com>
2021-10-17 16:36:36 +0530

    test case for three numbers input

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -23,6 +23,12 @@ class StringCalculatorShould {
 	}
     
     
+    @Test
+    public void testThreeNumbers(){
+    	assertEquals(6, StringCalculator.add("1,2,3"));
+    }
+    
+    
     
     
     
